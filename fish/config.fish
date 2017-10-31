969defe7cf592d008484577eacf4ffcf22698e0e
Christian Poveda <christianpoveda@protonmail.com>
2017-10-30 21:06:42 -0500

    changed rust source path to stable

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set -gx EDITOR "nvim"
 
 set -gx GOPATH $HOME/.go
 set -gx PREFIX $HOME/.yarn_packages
-set -gx RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+set -gx RUST_SRC_PATH $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
 set -gx PATH $PREFIX/bin $PATH
 set -gx PATH $HOME/.local/bin $PATH
@@ -13,6 +13,10 @@ alias nano "nvim"
 
 function check-sync
     watch grep -e Dirty: -e Writeback: /proc/meminfo
+end
+
+function grability
+    /home/christian/Workspace/rappi/utils/run.py $argv
 end
 
 tabs 4
