942a0e48e96de4ee772d8c4a748f5fe79853b629
Christian Poveda <christianpoveda@protonmail.com>
2018-03-29 15:29:35 -0500

    link now removes old configs and then creates symlinks

--- link.sh
+++ link.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+rm -r ~/.config/nvim
 ln -sf $(pwd)/nvim ~/.config/nvim
+
+rm -r ~/.config/terminator
+ln -sf $(pwd)/terminator ~/.config/terminator
+
+rm ~/.zshrc ~/.zpreztorc
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
 ln -sf $(pwd)/zsh/zpreztorc ~/.zpreztorc
