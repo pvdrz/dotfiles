19c6dd5e5fc6d89a6a4f8aa203124861f9a0e355
Christian Poveda <christianpoveda@protonmail.com>
2018-11-13 10:33:30 -0500

    Remove tmux and alacritty

--- link.sh
+++ link.sh
@@ -4,9 +4,3 @@ ln -sf $(pwd)/nvim ~/.config/nvim
 
 rm -r ~/.config/fish
 ln -sf $(pwd)/fish ~/.config/fish
-
-rm -r ~/.config/alacritty
-ln -sf $(pwd)/alacritty/ ~/.config/alacritty
-
-rm -r ~/.tmux.conf
-ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
