e126a7a313f592a3ecffd5d64a8076cc50cbde76
Christian Poveda <git@christianpoveda.xyz>
2020-06-11 16:29:11 -0500

    use elixir-ls

--- nvim/plugins/nvim_lsp.lua
+++ nvim/plugins/nvim_lsp.lua
@@ -1,11 +1,22 @@
 require'nvim_lsp'.rust_analyzer.setup({
     capabilities = {
-      textDocument = {        completion = {
-
+      textDocument = {
+          completion = {
             completionItem = {
                 snippetSupport = false
             }
         }
     }
-}
-})
+}})
+
+require'nvim_lsp'.elixirls.setup({
+    cmd = { "elixir-ls" },
+    capabilities = {
+      textDocument = {
+          completion = {
+            completionItem = {
+                snippetSupport = false
+            }
+        }
+    }
+}})
