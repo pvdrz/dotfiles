f603c2b7173d42bf34b0ab0350c5be206c72ba5b
Christian Poveda <git@christianpoveda.xyz>
2021-02-19 13:18:18 +0100

    remove lsp debug config

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,9 +9,3 @@ require('plugins.lexima')
 require('plugins.lightline')
 require('plugins.deoplete')
 require('plugins.languageclient')
-vim.api.nvim_command([[
-let g:LanguageClient_loggingLevel = 'DEBUG'
-let g:LanguageClient_virtualTextPrefix = ''
-let g:LanguageClient_loggingFile =  expand('~/LanguageClient.log')
-let g:LanguageClient_serverStderr = expand('~/LanguageServer.log')
-]])
