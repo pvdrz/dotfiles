a6675c48631b394a7444b0d209d460aab8599abf
Christian Poveda <git@pvdrz.com>
2021-04-16 20:10:49 -0500

    fix tabs

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -15,14 +15,22 @@ vim.api.nvim_set_option('whichwrap', '<,>,[,]')
 -- delete trailing whitespaces on save
 -- FIXME: Remove vimscript (Blocked on: https://github.com/neovim/neovim/pull/12378).
 vim.api.nvim_command('autocmd BufWritePre * :FixWhitespace')
--- set number of spaces for tab
-vim.api.nvim_set_option('ts', 4)
--- set number of spaces for reindent
+-- set number of spaces per tab
+vim.api.nvim_set_option('tabstop', 4)
+vim.api.nvim_buf_set_option(0, 'tabstop', 4)
+-- set number of spaces per tab when editing
+vim.api.nvim_set_option('softtabstop', 4)
+vim.api.nvim_buf_set_option(0, 'softtabstop', 4)
+-- set number of spaces for autoindent
 vim.api.nvim_set_option('shiftwidth', 4)
+vim.api.nvim_buf_set_option(0, 'shiftwidth', 4)
+-- expand dabs with spaces
+vim.api.nvim_set_option('expandtab', true)
+vim.api.nvim_buf_set_option(0, 'expandtab', true)
 -- set alternative number of spaces for latex and elixir
 -- FIXME: Remove vimscript (Blocked on: https://github.com/neovim/neovim/pull/12378).
-vim.api.nvim_command('autocmd Filetype tex setlocal ts=2 sw=2')
-vim.api.nvim_command('autocmd Filetype elixir setlocal ts=2 sw=2')
+vim.api.nvim_command('autocmd Filetype tex setlocal tabstop=2 softtabstop=2 shiftwidth=2')
+vim.api.nvim_command('autocmd Filetype elixir setlocal tabstop=2softtabstop=2 shiftwidth-2')
 -- use spaces instead of tabs
 vim.api.nvim_set_option('expandtab', true)
 -- adjust to the next indentation level
