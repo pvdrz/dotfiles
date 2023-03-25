bfe2c994e8c19f6a87a1e5d29d8e801748c4d9e3
Christian Poveda <git@pvdrz.com>
2023-03-25 18:18:33 -0500

    replace double quotes by single ones

--- nvim/lua/config/nvim-lspconfig.lua
+++ nvim/lua/config/nvim-lspconfig.lua
@@ -26,7 +26,7 @@ vim.diagnostic.config({
 
 settings.setup({
   config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
-  local_settings_dir = ".nlsp-settings",
+  local_settings_dir = '.nlsp-settings',
   local_settings_root_markers_fallback = { '.git' },
   append_default_schemas = true,
   loader = 'json'
