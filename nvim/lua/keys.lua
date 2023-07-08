b63cc92cbeec2f22193cb731edc621361844f309
Christian Poveda <git@pvdrz.com>
2023-07-08 15:39:29 -0500

    Don't paste with the middle mouse button

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -9,3 +9,10 @@ vim.keymap.set('', '<A-D>', '"_D', {})
 -- Navigate splits up and down
 vim.keymap.set('n', '<A-Up>', '<C-w><Up>', {})
 vim.keymap.set('n', '<A-Down>', '<C-w><Down>', {})
+
+-- Don't paste with the middle mouse button
+for _, prefix in ipairs({"", "2-", "3-", "4-"}) do
+  local lhs = "<" .. prefix .. "MiddleMouse>"
+  vim.keymap.set('n', lhs, "<Nop>", {})
+  vim.keymap.set('i', lhs, "<Nop>", {})
+end
