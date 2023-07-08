19b63d3253588beffc52f309bb95758fc822dc75
Christian Poveda <git@pvdrz.com>
2023-07-08 15:41:01 -0500

    Remove unused mappings

--- nvim/lua/config/vim-floaterm.lua
+++ nvim/lua/config/vim-floaterm.lua
@@ -12,19 +12,7 @@ vim.g.floaterm_opener = 'tabe'
 -- don't enter terminal mode after opening a temrina;
 vim.g.floaterm_autoinsert = false
 
-local function run_in_terminal(cmd)
-  local n = #vim.fn["floaterm#buflist#gather"]()
-  if n == 0 then
-    vim.cmd.FloatermNew()
-  end
-  vim.cmd.FloatermSend(cmd)
-end
-
-vim.keymap.set('n', '<C-c><C-c>', function() run_in_terminal("cargo check") end)
-vim.keymap.set('n', '<C-c><C-b>', function() run_in_terminal("cargo build") end)
-vim.keymap.set('n', '<C-c><C-t>', function() run_in_terminal("cargo test") end)
-vim.keymap.set('n', '<C-c><C-l>', function() run_in_terminal("cargo +nightly clippy") end)
-
+-- disable number column for terminal buffers.
 vim.api.nvim_create_autocmd('TermOpen', {
   pattern = '*',
   callback = function()
