05199f77f76f05032df4d2917e27aa5a4e304a82
Christian Poveda <git@pvdrz.com>
2024-01-09 10:56:52 -0500

    Change the underline for `DiagnosticUnnecessary`

--- nvim/lua/config/nord-nvim.lua
+++ nvim/lua/config/nord-nvim.lua
@@ -5,8 +5,12 @@ vim.g.nord_italic = false
 -- Load the colorscheme
 require('nord').set()
 
--- Set visual highlight as the underline for diagnostics  
-for _, level in pairs({'Error', 'Hint', 'Info', 'Warn'}) do
+-- Set visual highlight as the underline for diagnostics
+local diagnostic_highlight = 'Visual'
+
+for _, level in pairs({ 'Error', 'Hint', 'Info', 'Warn' }) do
   -- FIXME: Do this with the Lua API.
-  vim.cmd('highlight! link DiagnosticUnderline' .. level .. ' Visual')
+  vim.cmd('highlight! link DiagnosticUnderline' .. level .. ' ' .. diagnostic_highlight)
 end
+
+vim.cmd('highlight! link DiagnosticUnnecessary ' .. diagnostic_highlight)
